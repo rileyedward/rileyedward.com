61225ee5b6e9da802d6ebaddacad62859964ddaf
Riley Grotenhuis <riley@Rileys-Air.attlocal.net>
2024-09-10 22:19:00 -0500

    update

--- src/lib/components/landing/Showcase.svelte
+++ src/lib/components/landing/Showcase.svelte
@@ -1,24 +1,33 @@
-<div id="showcase" class="w-full py-12 bg-[#1f1f22]">
-	<div class="max-w-4xl mx-auto px-4 md:px-8">
-		<h1 class="text-3xl font-bold text-white mb-6 text-center">Checkout My Talk! 🚀</h1>
-		<p class="max-w-3xl mx-auto text-base text-gray-300 mb-8 text-center">
-			In this talk, I explore Laravel’s core features and its ecosystem of first-party packages,
-			showcasing how they enhance development and streamline your projects.
+<div id="showcase" class="w-full py-16 bg-gradient-to-b from-[#1f1f22] to-[#131315]">
+	<div class="max-w-5xl mx-auto px-6 md:px-10">
+		<h1 class="text-3xl font-extrabold text-white mb-4 text-center tracking-wide">
+			My tech talk on Laravel <div class="ml-2 inline-flex animate-bounce">🚀</div>
+		</h1>
+		<p class="max-w-2xl mx-auto text-md text-gray-400 mb-8 text-center leading-relaxed">
+			Discover Laravel’s core features and explore its ecosystem of first-party packages, 
+			uncovering how they enhance development and streamline your projects.
 		</p>
-		<div
-			class="relative overflow-hidden rounded-2xl border-2 border-zinc-500 shadow-2xl transition-transform duration-300"
-			style="padding-bottom: 56.25%;"
-		>
-			<iframe
-				class="absolute inset-0 w-full h-full"
-				src="https://www.youtube.com/embed/kHjGp0Xtinw?si=J1Y4H3wObjIP72gG"
-				title="YouTube video on Laravel's Core Features"
-				frameborder="0"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-				referrerpolicy="strict-origin-when-cross-origin"
-				allowfullscreen
-				loading="lazy"
-			/>
-		</div>
+		<a href="https://www.youtube.com/embed/kHjGp0Xtinw?si=J1Y4H3wObjIP72gG" target="_blank">
+			<div
+				class="relative overflow-hidden rounded-3xl border border-zinc-600 shadow-xl hover:shadow-2xl transform hover:scale-105 transition-all duration-300"
+				style="padding-bottom: 56.25%;"
+			>
+				<!-- Dark Overlay with Gradient -->
+				<div class="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-50 hover:opacity-0 z-30 bg-neutral"></div>
+				<div class="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-50 hover:opacity-0 z-30 bg-neutral"></div>
+
+				<!-- Video Container -->
+				<iframe
+					class="absolute inset-0 w-full h-full z-10"
+					src="https://www.youtube.com/embed/kHjGp0Xtinw?si=J1Y4H3wObjIP72gG"
+					title="YouTube video on Laravel's Core Features"
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+					referrerpolicy="strict-origin-when-cross-origin"
+					allowfullscreen
+					loading="lazy"
+				></iframe>
+			</div>
+		</a>
 	</div>
 </div>
