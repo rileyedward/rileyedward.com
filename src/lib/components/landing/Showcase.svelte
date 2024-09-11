b4452f36e79fc373ff3cd94d8c95dce563c45c30
Riley Grotenhuis <riley@Rileys-Air.attlocal.net>
2024-09-10 22:29:55 -0500

    showcase

--- src/lib/components/landing/Showcase.svelte
+++ src/lib/components/landing/Showcase.svelte
@@ -1,15 +1,15 @@
 <div id="showcase" class="w-full py-16 bg-gradient-to-b from-[#1f1f22] to-[#131315]">
 	<div class="max-w-5xl mx-auto px-6 md:px-10">
-		<h1 class="text-3xl font-extrabold text-white mb-4 text-center tracking-wide">
-			My tech talk on Laravel <div class="ml-2 inline-flex animate-bounce">🚀</div>
+		<h1 class="text-2xl md:text-3xl font-extrabold text-white mb-4 text-center tracking-wide">
+			My talk on Laravel <div class="ml-2 inline-flex animate-bounce">🚀</div>
 		</h1>
-		<p class="max-w-2xl mx-auto text-md text-gray-400 mb-8 text-center leading-relaxed">
-			Discover Laravel’s core features and explore its ecosystem of first-party packages, 
-			uncovering how they enhance development and streamline your projects.
+		<p class="max-w-xl mx-auto text-md text-gray-400 mb-8 text-center leading-relaxed">
+			Deep dive with me into the Laravel framework and its ecosystem that makes it an 
+			excellent choice for building web applications.
 		</p>
 		<a href="https://www.youtube.com/embed/kHjGp0Xtinw?si=J1Y4H3wObjIP72gG" target="_blank">
 			<div
-				class="relative overflow-hidden rounded-3xl border border-zinc-600 shadow-xl hover:shadow-2xl transform hover:scale-105 transition-all duration-300"
+				class="relative overflow-hidden rounded-3xl border border-zinc-600 shadow-xl hover:shadow-2xl transform hover:scale-105 transition-all duration-300 max-w-[750px] mx-auto"
 				style="padding-bottom: 56.25%;"
 			>
 				<!-- Dark Overlay with Gradient -->
