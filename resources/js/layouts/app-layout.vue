0af0fe66e0014e6ec125ec45c17d7df0b54529e1
Riley Grotenhuis <rileygrotenhuis@Mac.attlocal.net>
2025-06-14 12:31:19 -0500

    layout page titles

--- resources/js/layouts/app-layout.vue
+++ resources/js/layouts/app-layout.vue
@@ -1,5 +1,16 @@
+<script setup lang="ts">
+import { Props } from '@/types/layouts/app-layout';
+import { Head as InertiaHead } from '@inertiajs/vue3';
+
+withDefaults(defineProps<Props>(), {
+    pageTitle: 'Riley Edward',
+});
+</script>
+
 <template>
-    <div>
+    <inertia-head :title="pageTitle" />
+
+    <main>
         <slot />
-    </div>
+    </main>
 </template>
