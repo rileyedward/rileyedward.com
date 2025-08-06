21e76280078ef818b2c7d7bb3661fd4163d3d9e0
Riley Grotenhuis <rileygrotenhuis@Mac.attlocal.net>
2025-08-05 21:21:16 -0500

    remove unused route

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,6 @@ Route::get('/portfolio', function () {
     return inertia('portfolio');
 })->name('portfolio.index');
 
-Route::get('/blog', function () {
-    return inertia('blog');
-})->name('blog.index');
-
 Route::controller(BlogController::class)
     ->prefix('blog')
     ->name('blog.')
